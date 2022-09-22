0faa167bc4222b19da855606b2522f5b11d229d4
cristianvitor01 <cristianvitoralves@gmail.com>
2022-09-21 22:13:13 -0300

    corrected Procfile

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ce4288ac61ec58ef3cb244e94d46ec8c'
+# app.config['SECRET_KEY'] = 'ce4288ac61ec58ef3cb244e94d46ec8c'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
